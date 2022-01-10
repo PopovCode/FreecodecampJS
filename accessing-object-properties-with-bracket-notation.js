56faa43845be0e2084a634f5d5dd9d9f795c1460
Popov A.S <antoxamad@gmail.com>
2022-01-10 12:26:51 +0300

    Сompleted the next tasks from freecodecamp.

---
+++ accessing-object-properties-with-bracket-notation.js
@@ -0,0 +1,10 @@
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"];
---
+++ build-javascript-objects.js
@@ -0,0 +1,6 @@
+const myDog = {
+  name: "Dina",
+  legs: 4,
+  tails: 1,
+  friends: ["Sofa", "Bonya"]
+};
